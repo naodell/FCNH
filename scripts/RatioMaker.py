4ab29fc5d7cab4070665a8aa3ccbd9174caaf78b
naodell <naodell@gmail.com>
2013-08-22 19:43:22 -0500

    script for getting b-jet mc efficiencies seems to work

--- scripts/RatioMaker.py
+++ scripts/RatioMaker.py
@@ -5,19 +5,17 @@ import subprocess
 import ROOT as r
 
 canvas  = r.TCanvas('canvas', 'canvas', 800, 600)
-inFile  = r.TFile('histos/fcnh_cut1_2012_20130821_164730.root', 'OPEN')
+inFile  = r.TFile('histos/fcnh_cut1_2012_20130822_183318.root', 'OPEN')
 inPath  = 'inclusive/ttbar'
+#inFile  = r.TFile('histos/fcncHistograms_cut1.root', 'OPEN')
+#inPath  = 'inclusive/TEST'
 outFile = r.TFile('histos/bEff_ttbar_2012.root', 'RECREATE') 
-outPath = '~/work/plots/bTag_eff'
+outPath = 'plots/bTag_eff'
 
 r.gROOT.SetBatch()
 r.gStyle.SetOptStat(0)
 
-h2_Fakes2D  = []
-ptBins      = [10., 15., 20., 25., 30., 35., 50., 80., 120., 250.];
-etaBins     = [-2.5, -2., -1.479, -1., 0., 1., 1.479, 2., 2.5];
-
-ratioDict   = {'bTagEff':('BTruthNumerPt', 'BTruthDenomPt'), 'bMistagEff':('BMistagNumerPt', 'BTruthDenomPt')}
+ratioDict   = {'bTagEff':('BTruthNumerPt', 'BTruthDenomPt'), 'bMistagEff':('BMistagNumerPt', 'BMistagDenomPt')}
 
 canvas.SetGridx()
 canvas.SetGridy()
@@ -25,17 +23,17 @@ canvas.SetGridy()
 for key,value in ratioDict.iteritems():
 
     print 'Producing ratio plot {}'.format(key)
-    # pt fakes
+
     h1_Numer = inFile.GetDirectory(inPath).Get('h1_{0}'.format(value[0]))
     h1_Denom = inFile.GetDirectory(inPath).Get('h1_{0}'.format(value[1]))
 
     g_Eff = r.TGraphAsymmErrors()
     g_Eff.Divide(h1_Numer, h1_Denom)
-    g_Eff.SetName('g_FakePt_' + cat)
-    g_Eff.SetTitle(cat + ' fake rate;p_{T};#varepsilon')
-    g_Eff.Draw('AP')
+    g_Eff.SetName('g_{0}'.format(key))
+    g_Eff.SetTitle('{0};p_{{T}};#varepsilon'.format(key))
+    #g_Eff.Draw('ACP')
 
-    canvas.Print('{0}/{1}.png'.format(outPath, key)
+    #canvas.Print('{0}/{1}.png'.format(outPath, key))
 
     outFile.Add(g_Eff)
 
